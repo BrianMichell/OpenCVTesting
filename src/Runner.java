f2fb8872506b86529423fa804938b8de02f89e9a
Brian <Brian@WOPR>
2017-06-10 13:23:54 -0500

    Updated to "stream" a video to the Swing display.

--- src/Runner.java
+++ src/Runner.java
@@ -4,24 +4,20 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JSlider;
 import javax.swing.SwingUtilities;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
 
 @SuppressWarnings("unused")
 public class Runner {
-	
+
 	private static Mat image; // A saved version of the picture.
 	private static Mat tmp;
 	private static int hueMin;
@@ -30,85 +26,56 @@ public class Runner {
 	private static int satMax;
 	private static int valMin;
 	private static int valMax;
-	
 
 	public void graphics() {
-		
+
 		System.out.println("This is the HSV file");
 
 		SwingUtilities.invokeLater(() -> { // Lambda expression to start the
 											// Swing program
-			JFrame frame = new JFrame("OpenCV image testing"); // Creates the framework and names it.
-			
-			GridLayout layout = new GridLayout(0,3);
+			JFrame frame = new JFrame("OpenCV image testing"); // Creates the
+																// framework and
+																// names it.
+
+			GridLayout layout = new GridLayout(0, 3);
 			frame.setLayout(layout);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Allows the program to stop when the "X" button is pressed
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Allows the
+																	// program
+																	// to stop
+																	// when the
+																	// "X"
+																	// button is
+																	// pressed
 			BufferedImage img = null; // A usable picture for Swing
 
 			try {
-				img = Mat2BufferedImage(tmp); // Sets the picture to whatever's in the matrix
+				img = Mat2BufferedImage(tmp); // Sets the picture to whatever's
+												// in the matrix
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 
 			JLabel lbl = new JLabel(); // Where the picture will be placed
-			JLabel picture = new JLabel(); // Unmodified picture
-
-			// Slider to make quick adjustments based on the range
-			JSlider hueMinSlider = new JSlider(0, 255, 0);
-			JSlider hueMaxSlider = new JSlider(0,255,255);
-			JSlider satMinSlider = new JSlider(0,255,0);
-			JSlider satMaxSlider = new JSlider(0,255,255);
-			JSlider valMinSlider = new JSlider(0,255,0);
-			JSlider valMaxSlider = new JSlider(0,255,255);
-			
-			hueMinSlider.addChangeListener((e)->{
-				hueMin=hueMinSlider.getValue();
-				updateDisplay(frame,lbl);
-			});
-			hueMaxSlider.addChangeListener((e)->{
-				hueMax=hueMaxSlider.getValue();
-				updateDisplay(frame,lbl);
-			});
-			satMinSlider.addChangeListener((e)->{
-				satMin=satMinSlider.getValue();
-				updateDisplay(frame,lbl);
-			});
-			satMaxSlider.addChangeListener((e)->{
-				satMax=satMaxSlider.getValue();
-				updateDisplay(frame,lbl);
-			});
-			valMinSlider.addChangeListener((e)->{
-				valMin=valMinSlider.getValue();
-				updateDisplay(frame,lbl);
-			});
-			valMaxSlider.addChangeListener((e)->{
-				valMax=valMaxSlider.getValue();
-				updateDisplay(frame,lbl);
-			});
-			
 
 			lbl.setIcon(new ImageIcon(img));
-			
-			
-			frame.add(hueMaxSlider);
-			frame.add(new JLabel("<--- Max Hue Min --->"));
-			frame.add(hueMinSlider);
-			frame.add(satMaxSlider);
-			frame.add(new JLabel("<--- Max Sat Min --->"));
-			frame.add(satMinSlider);
-			frame.add(valMaxSlider);
-			frame.add(new JLabel("<--- Max Val Min --->"));
-			frame.add(valMinSlider);
+
 			frame.add(lbl);
-			frame.add(new JLabel(""));
-			JLabel original = new JLabel();
-			original.setIcon(new ImageIcon(Mat2BufferedImage(image)));
-			frame.add(original);
-	
-			frame.setLocationRelativeTo(null); // The program will open wherever.
+
+			frame.setLocationRelativeTo(null); // The program will open
+												// wherever.
 			frame.pack(); // Puts all the frame together
 			frame.setVisible(true); // Shows the frame
+
+			new Thread(() -> {
+				for (int i = 0; i < 10; i++) {
+					updateDisplay(frame, lbl);
+				}
+				try{
+					Thread.sleep(3000);
+				} catch (InterruptedException e){
+					e.printStackTrace();
+				}
+			}).start();
 
 		});
 
@@ -120,7 +87,7 @@ public class Runner {
 		image = Imgcodecs.imread("C:\\Users\\Brian\\Pictures\\tower.jpg");
 		tmp = new Mat(image.width(), image.cols(), image.type());
 		Imgproc.cvtColor(image, tmp, Imgproc.COLOR_BGR2HSV);
-		Core.inRange(tmp, new Scalar(0,100,0), new Scalar(50,255,255), tmp);
+		Core.inRange(tmp, new Scalar(0, 100, 0), new Scalar(50, 255, 255), tmp);
 
 	}
 
@@ -132,10 +99,12 @@ public class Runner {
 	 * @author Brian
 	 */
 	public static Mat hsvChange() {
-		Mat tmp = image.clone();
-		Imgproc.cvtColor(image, tmp, Imgproc.COLOR_BGR2HSV);
-		Core.inRange(tmp, new Scalar(hueMin,satMin,valMin), new Scalar(hueMax,satMax,valMax), tmp);
-		return tmp;
+
+		// Mat tmp = vid.grab();
+		// Imgproc.cvtColor(image, tmp, Imgproc.COLOR_BGR2HSV);
+		// Core.inRange(tmp, new Scalar(hueMin,satMin,valMin), new
+		// Scalar(hueMax,satMax,valMax), tmp);
+		return null;
 	}
 
 	/**
@@ -168,17 +137,29 @@ public class Runner {
 
 	/**
 	 * Update the image being displayed.
-	 * @param frm The canvas that needs updating.
-	 * @param label The holder for the picture.
+	 * 
+	 * @param frm
+	 *            The canvas that needs updating.
+	 * @param label
+	 *            The holder for the picture.
 	 */
-	private static void updateDisplay(JFrame frm, JLabel label){
-		Mat tmp = hsvChange().clone();
-		try {
-			label.setIcon(new ImageIcon(Mat2BufferedImage(tmp)));
-		} catch (Exception ex) {
-			ex.printStackTrace();
+	private static void updateDisplay(JFrame frm, JLabel label) {
+		VideoCapture vid = new VideoCapture(0);
+		vid.open(0);
+		if (!vid.isOpened()) {
+			System.out.println("This has failed!");
+		} else {
+			System.out.println("It's all good!");
+
+			Mat tmp = new Mat();
+			if (vid.read(tmp))
+				try {
+					label.setIcon(new ImageIcon((Mat2BufferedImage(tmp))));
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			frm.repaint();
 		}
-		frm.repaint();
 	}
 
 }
--- src/Setup.java
+++ src/Setup.java
@@ -15,40 +15,7 @@ public class Setup {
 	public static void main(String[] args) {
 		
 		Runner r = new Runner();
-		
-		System.out.println("This is the HSV testing file");
-
-		SwingUtilities.invokeLater(() -> {
-			JFrame frame = new JFrame("Setup");
-			JTextField txt = new JTextField();
-			JButton okay = new JButton("Okay");
-			JLabel label = new JLabel("Please input the max blur value you want and press Okay");
-
-			okay.addActionListener((e) -> {
-				if (txt.getText() != null) { // Just makes sure that there is
-												// something in the field
-					if (Integer.parseInt(txt.getText()) >= 2) {
-						r.setMaxVal(Integer.parseInt(txt.getText()));
-					} else {
-						r.setMaxVal(100);
-					}
-					frame.dispose();
-					r.graphics();
-				}
-			});
-
-			frame.getContentPane().add(txt, BorderLayout.NORTH);
-			frame.getContentPane().add(okay, BorderLayout.SOUTH);
-			frame.getContentPane().add(label, BorderLayout.CENTER);
-
-			frame.setLocationRelativeTo(null); // The program will just open
-												// wherever. You can change this
-												// if you want it to be
-												// somewhere specific.
-			frame.pack(); // Puts all the frame together
-			frame.setVisible(true); // Shows the frame
-
-		});
+		r.graphics();
 
 	}
 
